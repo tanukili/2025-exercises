de0d9ce1232a388033421876953355c59d689553
tanukili <backpacker320@gmail.com>
2025-06-04 15:42:43 +0800

    fix: 假資料圖片路徑、BrowserRouter basename

--- React/2025-ExpCamp-final/src/main.tsx
+++ React/2025-ExpCamp-final/src/main.tsx
@@ -3,11 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 
 import '@/assets/index.css'
-import App from './App.tsx'
+import App from '@/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.NODE_ENV === 'production' ? '/2025-exercises/React/2025-ExpCamp-final/' : '/'}>
       <App />
     </BrowserRouter>
   </StrictMode>,
