34a64b444378a2c94392946995011467cb0d3fc3
tanukili <backpacker320@gmail.com>
2025-08-11 17:28:44 +0800

    職涯諮詢 - chore: Brootstrap5、SCSS、Material icon

--- React/2025-work-way/src/App.jsx
+++ React/2025-work-way/src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <span class="material-symbols-outlined">face</span>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
