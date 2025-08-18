0a13a3d44ead1ee9e77270cc11bd0d7c6cb2b892
tanukili <backpacker320@gmail.com>
2025-08-18 16:06:22 +0800

    旅遊網站 - chore: 開發環境建立、套件安裝

--- React/2025-ZOBAA/src/main.jsx
+++ React/2025-ZOBAA/src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import '@/assets/main.scss'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
