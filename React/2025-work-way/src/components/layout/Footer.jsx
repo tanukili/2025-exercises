0ed4d536c2672465eac049bcd683637e41ee5034
tanukili <108317269+tanukili@users.noreply.github.com>
2025-08-19 08:43:24 +0800

    職涯諮詢 - feat: footer mobile 樣式

--- React/2025-work-way/src/components/layout/Footer.jsx
+++ React/2025-work-way/src/components/layout/Footer.jsx
@@ -2,37 +2,43 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <footer className="container text-center py-4">
-      <ul className="nav justify-content-center">
-        <li className="nav-item">
-          <Link to="plans" className="nav-link" aria-current="page">
-            服務方案
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="about" className="nav-link">
-            關於職旅
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="contact" className="nav-link">
-            聯絡我們
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="privacy-policy" className="nav-link">
-            隱私權政策
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="terms-of-service" className="nav-link">
-            用戶條款
-          </Link>
-        </li>
-      </ul>
-      <p className="text-secondary">
-        Copyright © 2025 職旅 workway. All rights reserved.
-      </p>
+    <footer className="border-top-linear text-center bg-natural-95 py-6 fs-8 fs-md-7">
+      <div className="container">
+        <ul className="nav justify-content-center">
+          <li className="nav-item mb-2">
+            <Link
+              to="plans"
+              className="nav-link link-primary"
+              aria-current="page"
+            >
+              服務方案
+            </Link>
+          </li>
+          <li className="nav-item mb-2">
+            <Link to="about" className="nav-link link-primary">
+              關於職旅
+            </Link>
+          </li>
+          <li className="nav-item mb-2">
+            <Link to="contact" className="nav-link link-primary">
+              聯絡我們
+            </Link>
+          </li>
+          <li className="nav-item mb-2">
+            <Link to="privacy-policy" className="nav-link link-primary">
+              隱私權政策
+            </Link>
+          </li>
+          <li className="nav-item mb-2">
+            <Link to="terms-of-service" className="nav-link link-primary">
+              用戶條款
+            </Link>
+          </li>
+        </ul>
+        <p className="text-natural-50 mb-0">
+          Copyright © 2025 職旅 workway. All rights reserved.
+        </p>
+      </div>
     </footer>
   );
 }
