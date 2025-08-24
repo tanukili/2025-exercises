b86bf3ffdc9822ec4dc5ab53839e87f8bc41408e
tanukili <108317269+tanukili@users.noreply.github.com>
2025-08-24 23:41:09 +0800

    職涯諮詢 - chore: 設定根路徑

--- React/2025-work-way/src/main.jsx
+++ React/2025-work-way/src/main.jsx
@@ -7,9 +7,11 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 import App from "@/App.jsx";
 
+const basename = import.meta.env.BASE_URL; // 去掉尾端斜線以保險
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </StrictMode>,
